803282ad856a2aa179d7d3a4b7ebb155644ca5c8
ntchble <yaaam0330@gmail.com>
2024-09-21 21:51:27 +0900

    課題用ディレクトリ「kadai_007」をプッシュ

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>連想配列を作って値とキーを出力しよう</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列を作成する
+    $data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    // 値を出力する
+    print_r($data);
+    ?>
+  </p>
+</body>
+
+</html>
