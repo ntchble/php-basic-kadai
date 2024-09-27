90d9153d119828798ef64fe05b5df78efbbad331
ntchble <yaaam0330@gmail.com>
2024-09-27 19:53:30 +0900

    課題用ディレクトリ「kadai_011」をプッシュ

---
+++ kadai_011/foreach.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>foreach文を使って連想配列の値とキーを出力しよう</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $contents = [
+      '名前' => '玉ねぎ',
+      '値段' => 200,
+      '産地' => '北海道'
+    ];
+
+    foreach ($contents as $key => $value) {
+      echo "{$key} : {$value}<br>";
+    }
+    ?>
+  </p>
+</body>
